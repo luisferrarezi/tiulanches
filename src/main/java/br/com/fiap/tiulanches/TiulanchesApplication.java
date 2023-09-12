5199cc50d07d6d21d71261e4b9e382e360c714ae
Luis <luisferrarezi@hotmail.com>
2023-09-12 19:03:35 -0300

    Created tiulanches project

---
+++ src/main/java/br/com/fiap/tiulanches/TiulanchesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tiulanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiulanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiulanchesApplication.class, args);
+	}
+
+}
