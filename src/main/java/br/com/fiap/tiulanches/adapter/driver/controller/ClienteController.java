97e91bb5cd66c971ad3114fb0f980a87ea69ab3d
Luis <luisferrarezi@hotmail.com>
2023-10-03 18:00:36 -0300

    Implemented Doc Api by swagger to Cliente

--- src/main/java/br/com/fiap/tiulanches/adapter/driver/controller/ClienteController.java
+++ src/main/java/br/com/fiap/tiulanches/adapter/driver/controller/ClienteController.java
@@ -22,6 +22,7 @@ import br.com.fiap.tiulanches.adapter.driven.service.ClienteService;
 import br.com.fiap.tiulanches.adapter.infra.exception.ErroValidacao;
 import br.com.fiap.tiulanches.core.domain.dto.ClienteDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
@@ -42,6 +43,9 @@ public class ClienteController {
 	@Operation(summary = "Lista todos os clientes cadastrados", 
 	           description = "O retorno é paginado e o padrão são 10 registros por página", 
 	           tags = {"Cliente"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucesso, lista todos os clientes em paginação")
+	})	
 	public ResponseEntity<Page<ClienteDto>> consultar(@ParameterObject @PageableDefault(size=10) Pageable paginacao){
 		logger.info("Consultar clientes");
 		
@@ -51,7 +55,18 @@ public class ClienteController {
 	}
 	
 	@GetMapping("/{cpf}")
-	public ResponseEntity<ClienteDto> detalhar(@ParameterObject @PathVariable @NotNull String cpf){
+	@Operation(summary = "Retorna dados do cliente", 
+    		   description = "Retorna todos os dados do cliente do CPF informado", 
+    		   tags = {"Cliente"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucesso, retorna dados do cliente"),
+			@ApiResponse(responseCode = "404", description = "Falha, cliente não encontrado")
+	})		
+	public ResponseEntity<ClienteDto> detalhar(@ParameterObject 
+			                                   @PathVariable 
+			                                   @NotNull 
+			                                   @Schema(description = "CPF do cliente sem formatação", example = "70636213005", required = true, maxLength = 11) 
+											   String cpf){
 		logger.info("Consultar cliente pelo CPF: " + cpf);
 		
 		ClienteDto cliente = service.consultaClienteByCpf(cpf);
@@ -60,6 +75,13 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@Operation(summary = "Cadastra o cliente", 
+			   description = "Cria o cliente e retorna o registro cadastrado", 
+			   tags = {"Cliente"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Sucesso, cliente cadastrado"),
+			@ApiResponse(responseCode = "400", description = "Falha, não cadastra o cliente por faltar informação ou com informação errada")
+	})			
 	public ResponseEntity<Object> cadastrar(@ParameterObject @RequestBody @Valid ClienteDto dto, UriComponentsBuilder uriBuilder){
 		logger.info("Incluir cliente");
 		
@@ -75,7 +97,18 @@ public class ClienteController {
 	}
 	
 	@PutMapping("/{cpf}")
-	public ResponseEntity<ClienteDto> alterar(@ParameterObject @PathVariable @NotNull String cpf, @RequestBody @Valid ClienteDto dto){
+	@Operation(summary = "Altera dados do cliente", 
+			   description = "Altera dados do cliente e retorna eles", 
+			   tags = {"Cliente"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucesso, altera os dados do cliente e retorna eles"),
+			@ApiResponse(responseCode = "404", description = "Falha, cliente não encontrado")
+	})			
+	public ResponseEntity<ClienteDto> alterar(@ParameterObject 
+			    							  @PathVariable 
+			    							  @NotNull
+			    							  @Schema(description = "CPF do cliente sem formatação", example = "70636213005", required = true, maxLength = 11)
+			    							  String cpf, @RequestBody @Valid ClienteDto dto){
 		logger.info("Alterar cliente pelo CPF: " + cpf);
 		
 		ClienteDto clienteAlterado = service.alterarCliente(cpf, dto);		
@@ -84,11 +117,18 @@ public class ClienteController {
 	}	
 	
 	@DeleteMapping("/{cpf}")
+	@Operation(summary = "Exclui dados do cliente", 
+	   		   description = "Exclui dados do cliente", 
+	   		   tags = {"Cliente"})	
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "204", description = "Sucesso, cliente apagado"),
 			@ApiResponse(responseCode = "404", description = "Cliente não encontrado na base de dados")
 	})
-	public ResponseEntity<ClienteDto> excluir(@ParameterObject @PathVariable @NotNull String cpf){
+	public ResponseEntity<ClienteDto> excluir(@ParameterObject 
+			  								  @PathVariable 
+			  								  @NotNull
+			  								  @Schema(description = "CPF do cliente sem formatação", example = "70636213005", required = true, maxLength = 11)
+			  								  String cpf){
 		logger.info("Excluir cliente pelo CPF: " + cpf);
 		
 		service.excluirCliente(cpf);		
