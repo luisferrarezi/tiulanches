f59e5c5dabc397310bc25c57ffbaf54606f7b430
Luis <luisferrarezi@hotmail.com>
2024-04-13 15:28:14 -0300

    Conclusao implementacao kafka

--- src/main/java/br/com/fiap/tiulanches/adapter/controller/PagamentoController.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.tiulanches.adapter.controller;
-
-import br.com.fiap.tiulanches.adapter.repository.pagamento.ConsultaPagamentoDto;
-import br.com.fiap.tiulanches.adapter.repository.pagamento.PagamentoDto;
-
-public interface PagamentoController {
-	public ConsultaPagamentoDto consultaPagamento(Long idPedido);	
-	public void registra(PagamentoDto dto);
-}
--- src/main/java/br/com/fiap/tiulanches/adapter/controller/PainelPedidoController.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.tiulanches.adapter.controller;
-
-import java.util.List;
-
-import br.com.fiap.tiulanches.adapter.repository.painelpedido.PainelPedidoDto;
-
-public interface PainelPedidoController {
-	public List<PainelPedidoDto> consultaPainelPedido();
-}
