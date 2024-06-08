24c4fa5152bcdcd230ea7f08769e6325df8167d5
Luis <luisferrarezi@hotmail.com>
2024-06-08 11:54:09 -0300

    ajuste

--- src/main/java/br/com/fiap/tiulanches/core/entity/cliente/Cliente.java
+++ src/main/java/br/com/fiap/tiulanches/core/entity/cliente/Cliente.java
@@ -4,13 +4,10 @@ import br.com.fiap.tiulanches.core.annotation.Cpf;
 import br.com.fiap.tiulanches.adapter.repository.cliente.ClienteDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -29,53 +26,53 @@ public class Cliente {
 	@Id	
 	@Size(max=11, min=11)
 	@Cpf
-	@Schema(description = "CPF do cliente sem formatação", example = "70636213005", required = true, maxLength = 11, minLength = 11)
+	@Schema(description = "CPF do cliente sem formatação", example = "70636213005", maxLength = 11, minLength = 11)
 	private String cpf;
 	
 	@NotBlank
 	@Size(max=60)
-	@Schema(description = "Nome do cliente", example = "Luis Antonio", required = true, maxLength = 60)
+	@Schema(description = "Nome do cliente", example = "Luis Antonio", maxLength = 60)
 	private String nome;
 	
 	@NotBlank
 	@Email
 	@Size(max=60)
-	@Schema(description = "Email do cliente", example = "luisantonio@gmail.com", required = true, maxLength = 60)
+	@Schema(description = "Email do cliente", example = "luisantonio@gmail.com", maxLength = 60)
 	private String email;
 
 	@NotBlank
 	@Size(max=100)
-	@Schema(description = "Endereço do cliente", example = "Rua Augustina", required = true, maxLength = 100)
+	@Schema(description = "Endereço do cliente", example = "Rua Augustina", maxLength = 100)
 	private String endereco;
 
 	@NotBlank
 	@Size(max=10)
-	@Schema(description = "Número endereço do Cliente", example = "1234", required = true, maxLength = 10)
+	@Schema(description = "Número endereço do Cliente", example = "1234", maxLength = 10)
 	private String numero;
 
 	@NotBlank
 	@Size(max=100)
-	@Schema(description = "Bairro do cliente", example = "Santa Luzia", required = true, maxLength = 100)
+	@Schema(description = "Bairro do cliente", example = "Santa Luzia", maxLength = 100)
 	private String bairro;
 
 	@NotBlank
 	@Size(max=100)
-	@Schema(description = "Cidade do cliente", example = "Potirendaba", required = true, maxLength = 100)
+	@Schema(description = "Cidade do cliente", example = "Potirendaba", maxLength = 100)
 	private String cidade;
 
 	@NotBlank
 	@Size(max=2)
-	@Schema(description = "Estado cidade do cliente", example = "SP", required = true, maxLength = 2)
+	@Schema(description = "Estado cidade do cliente", example = "SP", maxLength = 2)
 	private String estado;
 
 	@NotBlank
 	@Size(max=9)
-	@Schema(description = "CEP do cliente", example = "11111-000", required = true, maxLength = 9)
+	@Schema(description = "CEP do cliente", example = "11111-000", maxLength = 9)
 	private String cep;
 
 	@NotBlank
 	@Size(max=15)
-	@Schema(description = "Telefone do cliente", example = "(11) 99999-9999", required = true, maxLength = 15)
+	@Schema(description = "Telefone do cliente", example = "(11) 99999-9999", maxLength = 15)
 	private String telefone;
 
 	public void atualizar(ClienteDto cliente) {
